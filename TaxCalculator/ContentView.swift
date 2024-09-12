7dc304114229159d5694ec32a19aee7af3505dac
serijun1214 <serijun.1214+github@gmail.com>
2024-09-13 05:00:31 +0900

    消費税計算ロジックを実装

--- TaxCalculator/ContentView.swift
+++ TaxCalculator/ContentView.swift
@@ -10,17 +10,17 @@ import SwiftUI
 struct ContentView: View {
     
     @State var inputText = ""
+    @State var tax8 = 0.0
+    @State var tax10 = 0.0
+    
     
     var body: some View {
         VStack(spacing: 20) {
             TextField("ここに文字を入力", text: $inputText)
-            
-            Button("計算"){
-            
-            }
-            Text("価格：　")
-            Text("消費税8%：　")
-            Text("消費税10%：　")
+                .keyboardType(.numberPad)
+            Text("価格： \(inputText)")
+            Text("消費税8%： \((Double(inputText) ?? 0) * 0.08)")
+            Text("消費税10%： \((Double(inputText) ?? 0) * 0.1)")
         }
         .padding()
     }
