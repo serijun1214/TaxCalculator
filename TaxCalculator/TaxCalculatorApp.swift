5dcd88e61510e9f9c307a98d39b6b4a4b57085d8
serijun1214 <serijun.1214+github@gmail.com>
2024-09-12 23:23:11 +0900

    Initial Commit

---
+++ TaxCalculator/TaxCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCalculatorApp.swift
+//  TaxCalculator
+//
+//  Created by 瀬利純樹 on 2024/09/12.
+//
+
+import SwiftUI
+
+@main
+struct TaxCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
